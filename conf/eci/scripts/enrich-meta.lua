0d0f8a36c9c8db9a3402d1217d33bf45b37ca2ee
erenming <erenming233@gmail.com>
2022-03-28 15:50:29 +0800

    fix parser&lua script

--- conf/eci/scripts/enrich-meta.lua
+++ conf/eci/scripts/enrich-meta.lua
@@ -3,16 +3,36 @@ function add_cpuset(tag, timestamp, record)
 
     -- container's cpuset file path to extract containerID
     local cname = new_record["__pri_id"]
+    if cname == nil then
+        print(dump(new_record))
+        return -1, timestamp, new_record
+    end
 
     -- get cpuset from shared emptyDir
     local root_path = "/erda/containers/"
     --local root_path = "testdata/eci/containers/"
     local file = io.open(root_path .. cname .. "/cpuset", "r")
     if file == nil then
+        print(dump(new_record))
         return -1, timestamp, new_record
     end
     new_record["__pri_cpuset"] = file:read()
     file:close()
 
     return 1, timestamp, new_record
+end
+
+function dump(o)
+    if type(o) == 'table' then
+        local s = '{ '
+        for k, v in pairs(o) do
+            if type(k) ~= 'number' then
+                k = '"' .. k .. '"'
+            end
+            s = s .. '[' .. k .. '] = ' .. dump(v) .. ','
+        end
+        return s .. '} '
+    else
+        return tostring(o)
+    end
 end
